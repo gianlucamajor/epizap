d073d14b607c8f715e99c441178662a27c66a581
Gianluca Major <gianlucamajor@gmail.com>
2024-10-25 16:33:56 -0300

    reading bam file with pysam lib

--- bin/bam_handler.py
+++ bin/bam_handler.py
@@ -1,13 +1,23 @@
 import pysam
 
-bFilePath = "results/mapping/CCC_severe_a.bam"
+bFilePath = "results/mapping/asympto_a.bam"
+# bFilePath = "results_19_10_2024/mapping/control_and_chagasic_patients.bam"
+
 
 pysam.index(bFilePath)
 samfile = pysam.AlignmentFile(bFilePath, "rb")
-
+new_samfile = pysam.AlignmentFile("new_asympto_a.bam", "wb", template=samfile)
+primary_reads = []
 for read in samfile.fetch():
     if not read.is_secondary:
-        print(read.get_tags())
-        # print(read)
+        primary_reads.append(read)
+        new_samfile.write(read)
 
+new_samfile.close()    
 samfile.close()
+
+print(len(primary_reads))
+for pr in primary_reads:
+
+    print(pr.get_tag("AS"), pr.get_tag("XS"), pr.get_tags())
+
