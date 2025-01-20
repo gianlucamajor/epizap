dca3912e492fe709762546b54620a4b746466b31
Gianluca <gianlucamajor@gmail.com>
2025-01-20 09:49:42 -0300

    adding features to create report

--- bin/graph_analyse.py
+++ bin/graph_analyse.py
@@ -38,9 +38,9 @@ def get_epitopes(ept_file):
         ept_id = complete_segment_id.rsplit("-",1)[1]
         seg_inserts_id = seg_inserts_and_pep_id.rsplit("-",1)[0]
 
-        # print(seg_inserts_id, ept_id)
+        # print(complete_segment_id, seg_inserts_and_pep_id, seg_inserts_id, ept_id)
 
-        ept_seg = ept_by_segments.get(seg_inserts_id)
+        ept_seg = ept_by_segments.get(seg_inserts_and_pep_id)
         if not ept_seg:
             epts = []
             
@@ -48,7 +48,7 @@ def get_epitopes(ept_file):
             ept_element['id'] = ept_id
             ept_element['seq'] = str(seq)
             epts.append(ept_element)
-            ept_by_segments[seg_inserts_id] = epts
+            ept_by_segments[seg_inserts_and_pep_id] = epts
 
         else: 
             ept_element = {}
@@ -63,9 +63,10 @@ def get_epitopes(ept_file):
 
 def get_graph(graph_file, epitopes, output_file):
     G = pickle.load(open(graph_file, 'rb'))
-    print("Graph:",G)
-    
-    print(nx.number_connected_components(G))
+    # print("Graph:",G)
+    # print(nx.number_connected_components(G))
+
+    print(F"CC idx \t Nodes \t Edges \t Epitopes \t Unique Reads")
     
     all_cc = nx.connected_components(G)
 
@@ -94,19 +95,49 @@ def get_graph(graph_file, epitopes, output_file):
     
         final_ept_list.append(epts_seq_set_of_graph)
         
-        ## print the number of nodes, edges and epitopes in the connected component
+        ## START - print the number of nodes, edges and epitopes in the connected component
+        """
+        if idx == 6815:
 
-        # if idx == 5973:
-        #     print(idx, epts_seq_set_of_graph, sub_graph.nodes())
-        #     nx.draw_spring(sub_graph, with_labels=True)
-        #     plt.show()
+            unique_reads = set()
+            for n in sub_graph.nodes(data=True):
+                print("len:", len(n[1].get('reads', [])))
+                unique_reads.update(n[1].get('reads', []))
 
+            # To create a list of links to IGV  browser of segments and its insertions
+            igv_links = []
+            for n in sub_graph.nodes():
+                igv_links.append(_create_igv_link(n))
+            igv_links_str = ", ".join(igv_links)
 
-        print(idx, len(sub_graph.nodes()), len(sub_graph.edges()), len(epts_seq_set_of_graph))  
+            print(idx, epts_seq_set_of_graph, sub_graph.nodes())
+            # print(idx, len(sub_graph.nodes()), len(sub_graph.edges()), len(epts_seq_set_of_graph))  
+            print(F"{idx} \t {len(sub_graph.nodes())} \t {len(sub_graph.edges())} \t {len(epts_seq_set_of_graph)} \t {len(unique_reads)} \t {igv_links} \t {sub_graph.nodes()}")  
+            nx.draw_spring(sub_graph, with_labels=True)
+            plt.show()
+        """
+        ## END - print the number of nodes, edges and epitopes in the connected component
+
+        
         unique_reads = set()
         for n in sub_graph.nodes(data=True):
             unique_reads.update(n[1].get('reads', []))
-        print(f"Total unique reads in subgraph: {len(unique_reads)}")
+        # CC index, number of nodes, number of edges, number of epitopes, number of unique reads
+        
+        # To create a list of links to IGV  browser of segments and its insertions and MView
+        igv_links = []
+        mview_links = []
+        for n in sub_graph.nodes():
+            igv_links.append(_create_igv_link(n))
+            mview_links.append(_create_mview_link(n))
+        igv_links_str = ", ".join(igv_links)
+        mview_links_str = ", ".join(mview_links)
+
+
+        print(F"{idx} \t {len(sub_graph.nodes())} \t {len(sub_graph.edges())} \t {len(epts_seq_set_of_graph)} \t {len(unique_reads)} \t {', '.join(epts_seq_set_of_graph)} \t {igv_links_str} \t {mview_links_str}")
+        
+        
+        # print(f"Total unique reads in subgraph: {len(unique_reads)}")
         total_of_unique_reads_on_cc += len(unique_reads) 
         # for n in sub_graph.nodes(data=True):
         #     print(f"Node: {n[0]}, Reads: {len(n[1].get('reads'))}")
@@ -126,8 +157,46 @@ def get_graph(graph_file, epitopes, output_file):
     print(total_of_unique_reads_on_cc)
     # print(len(final_ept_set))
     # print(len(final_ept_list))
-       
+
+def _create_mview_link(segment):
+    local_host = "http://localhost:8080"
+    lbi_host = "http://projetos.lbi.iq.usp.br/trypanosoma/epizap"
+    mview_url = "mview/"
    
+    base_url = f"{lbi_host}/{mview_url}"
+
+    parts = segment.split('-')
+    if len(parts) < 5:
+        raise ValueError("Invalid segment ID format")
+    
+    number_of_peptides = int(parts[4])
+    if number_of_peptides <= 1:
+        return "There is no MSA"
+    
+    msa_id = f"{parts[0]}-{parts[1]}-{parts[2]}-{parts[3]}-{number_of_peptides}"
+    return f"{base_url}{msa_id}.html"
+
+
+def _create_igv_link(segment_id):
+    """
+        Expected is received something like this:
+        CM026600.1-29631-29710-758
+        And return a link like this:
+        http://localhost:8080/igv-webapp/?locus=CM026600.1:29631-29710
+    """
+
+    local_host = "http://localhost:8080"
+    lbi_host = "http://projetos.lbi.iq.usp.br/trypanosoma/epizap"
+    igv_webapp = "igv-webapp/?locus="
+
+    base_url = f"{lbi_host}/{igv_webapp}"
+    
+    
+    parts = segment_id.split('-')
+    if len(parts) < 3:
+        raise ValueError("Invalid segment ID format")
+    locus = f"{parts[0]}:{parts[1]}-{parts[2]}"
+    return f"{base_url}{locus}"
  
 def _get_output_file_name(input, outdir):
     outdir_path = os.path.dirname(input)
