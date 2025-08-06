3ced2b7d5d09901e04801f558c6fb6e8d0f292f5
Gianluca <gianlucamajor@gmail.com>
2025-08-06 11:08:32 -0300

    add featreus annotation on report . new report file as json format.

--- bin/epitope_reporter.py
+++ bin/epitope_reporter.py
@@ -1,6 +1,7 @@
 
 #!/usr/bin/env python
 
+import json
 import logging
 import click
 import networkx as nx
@@ -33,6 +34,7 @@ def main(graph_file:click.Path, outdir:click.Path, single_reads_not_allowed:bool
 
     cc_sorted_list = get_sorted_connected_components(graph)
     epitope_candidates_graph = []
+    report_json_epitopes_list = []
     report_epitopes_list = []
     for cc in cc_sorted_list:
         logger.debug(f"Connected component: {cc}")        
@@ -41,8 +43,10 @@ def main(graph_file:click.Path, outdir:click.Path, single_reads_not_allowed:bool
         features_cc = []
         epitope_candidates_cc = []
         igv_links = []
+        genomic_region_locus = []
         for node, attributes in cc.nodes(data=True):
             cc_ids = set()
+            genomic_region_locus.append(_get_locus_from_segment(node))
             igv_links.append(_create_igv_link(node))
             # print(f"Node: {node}")
             for attr, value in attributes.items():
@@ -57,14 +61,15 @@ def main(graph_file:click.Path, outdir:click.Path, single_reads_not_allowed:bool
                     epitope_candidates_cc = value
                     # print(f"  {attr}: {value}")
                 if attr == 'feature':
-                    features_cc.extend(value)
+                    features_cc.extend(parse_feature_string(value))
 
             if len(cc_ids) > 1:
                 raise Exception(f"CC {cc} has multiple component_ids: {cc_ids}")
             
         cc_id = cc_ids.pop()
 
-        msa_links_cc_= _create_mview_link(cc_id)
+        msa_links_cc_= _create_mview_link(cc_id, pepiteds_cc)
+        msa_page_name = _create_mview_name(cc_id, pepiteds_cc)
         
         if single_reads_not_allowed:
             if len(reads_cc) >= 2: 
@@ -76,15 +81,50 @@ def main(graph_file:click.Path, outdir:click.Path, single_reads_not_allowed:bool
         ept_candidantes_seq = []
         for e in epitope_candidates_cc:
             ept_candidantes_seq.append(str(e.seq))
+            # CC_idx	Nof_Nodes	Nof_Edges	Nof_Epitopes    Nof_Unique_peptides     Nof_Unique_Reads	Epitope_candidates     MSA_links    IGV_links   Features
+            # print(f"{cc_id}\t{cc.number_of_nodes()}\t{cc.number_of_edges()}\t{len(epitope_candidates_cc)}\t{len(pepiteds_cc)}\t{len(reads_cc)}\t{ept_candidantes_seq}\t{msa_links_cc_}\t{igv_links}\t{features_cc}")
+            # Print the information in tabular format
+            print(f"{e.id}\t{cc.number_of_nodes()}\t{cc.number_of_edges()}\t{len(epitope_candidates_cc)}\t{len(pepiteds_cc)}\t{len(reads_cc)}\t{str(e.seq)}\t{msa_links_cc_}\t{igv_links}\t{features_cc}")
 
-        
-        # CC_idx	Nof_Nodes	Nof_Edges	Nof_Epitopes    Nof_Unique_peptides     Nof_Unique_Reads	Epitope_candidates     MSA_links    IGV_links   Features
-        print(f"{cc_id}\t{cc.number_of_nodes()}\t{cc.number_of_edges()}\t{len(epitope_candidates_cc)}\t{len(pepiteds_cc)}\t{len(reads_cc)}\t{ept_candidantes_seq}\t{msa_links_cc_}\t{igv_links}\t{features_cc}")
+            # Create a JSON object with the same information
+            json_data = {
+                "ID": e.id,
+                "Number of Genomic Regions": cc.number_of_nodes(),
+                "Number of Peptides": len(pepiteds_cc), # unique peptides
+                "Number of Inserts": len(reads_cc), #unique reads
+                "Epitope": str(e.seq),
+                "MSA": msa_page_name,
+                "Genomic Region Locus": genomic_region_locus,
+                "Features": features_cc
+            }
+            report_json_epitopes_list.append(json_data)
+
+                
     
     output_file_name = _get_output_file_name(graph_file, outdir)
     _write_output_fasta_file(epitope_candidates_graph, f"{output_file_name}.fasta")
+    
+    # Write the JSON object to a file
+    with open(f"{output_file_name}.json", "w") as json_file:
+        json.dump(report_json_epitopes_list, json_file, indent=4)
         
 
+def parse_feature_string(features_str):
+    # Example input: ["WNWZ01000121.1-8261-8334 | KAF8303398.1 | tryptophanyl-tRNA synthetase, putative | 1.0", "WNWZ01000189.1-16368-16457 | pseudogene | tryptophanyl-tRNA synthetase, pseudonote | 0.8640776699029126"]
+    features = []
+    for feat in features_str: 
+        if feat:
+            parts = [p.strip() for p in feat.split('|')]
+            if len(parts) != 4:
+                raise ValueError(f"Invalid feature string format: {feat}")
+            features.append({
+        "genomic_region": parts[0],
+        "type": parts[1],
+        "description": parts[2],
+        "coverage": float(parts[3])
+            })
+    
+    return features
 
 def get_sorted_connected_components(graph):
     connected_component_list_result = []
@@ -95,19 +135,26 @@ def get_sorted_connected_components(graph):
 
     return connected_component_list_result
 
-def _create_mview_link(cc_id):
-    local_host = "http://localhost:8080"
-    lbi_host = "http://projetos.lbi.iq.usp.br/trypanosoma/epizap"
-    mview_url = "mview/"
-   
-    base_url = f"{local_host}/{mview_url}"
+def _create_mview_name(cc_id, number_of_peptides):
+    # Lonely peptides does have a MSA link
+    mview_name = ""
+    if len(number_of_peptides) > 1:
+        mview_name = f"cc-{cc_id}.html"
     
-    # number_of_peptides = int(parts[4])
-    # if number_of_peptides <= 1:
-    #     return "There is no MSA"
+    return mview_name
+
+def _create_mview_link(cc_id, number_of_peptides):
+    # Lonely peptides does have a MSA link
+    mview_name = ""
+    if len(number_of_peptides) > 1:
+        local_host = "http://localhost:8080"
+        lbi_host = "http://projetos.lbi.iq.usp.br/trypanosoma/epizap"
+        mview_url = "mview/"
     
-    msa_id = f"cc-{cc_id}"
-    return f"{base_url}{msa_id}.html"
+        base_url = f"{local_host}/{mview_url}"
+        msa_id = f"cc-{cc_id}"
+        mview_name = f"{base_url}{msa_id}.html"
+    return mview_name
 
 def _create_igv_link(node_name):
     """
